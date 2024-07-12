e53387c07f106ef357a78e2f9f89982619d1134b
aknur <95418973+aknurkappar@users.noreply.github.com>
2024-07-12 17:41:30 +0500

    Creating Vue2 project

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <AddClientForm/>
+  </div>
+</template>
+
+<script>
+import AddClientForm from './components/AddClientForm.vue'
+
+export default {
+  name: 'App',
+  components: {
+    AddClientForm
+  }
+}
+</script>
+
+<style>
+
+</style>
